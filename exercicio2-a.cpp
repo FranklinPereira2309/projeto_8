87dfc68ece54f381f12edd047fdc0856f3795f5a
Franklin Menezes <franklin.dev.full@gmail.com>
2023-09-11 10:54:18 -0300

    conclusao do exec2-a

--- exercicio2-a.cpp
+++ exercicio2-a.cpp
@@ -4,19 +4,14 @@ using namespace std;
 
 int main() {
     int arr[100] = {};
-    int arr2[100] = {};
-    int valor;
-    int frequencia;
-    
-for(int i = 0 ;  i < 20 ; i++) {
+       
+for(int i = 0 ;  i < 100 ; i++) {
     arr[i] = 1 +rand() % 20;
 }
 
-for(int i = 0 ; i < 20 ; i++) {
-    valor = arr[i];
-    frequencia = arr2[valor -1]++;
+for(int i = 0 ; i < 100 ; i++) {
+   cout << arr[i] << endl;
 
-    cout << "O numero:  " << valor << " Apareceu: " << frequencia +1 << " vez(es)"<< endl;
 }
 
 
